abf978f1b66adf39d24943f0b2794285a30ee82c
Aaron Heesakkers <aaronheesakkers@gmail.com>
2015-02-03 16:12:05 +0100

    Add includeTypes to filter shown annotations.

--- module.js
+++ module.js
@@ -4,40 +4,70 @@ define([
   'lodash',
   'moment',
   'components/panelmeta',
+  './config'
 ],
-function (angular, app, _, moment, PanelMeta) {
+function (angular, app, _, moment, PanelMeta, config) {
   'use strict';
 
   var module = angular.module('grafana.panels.annotations', []);
   app.useModule(module);
 
-  module.controller('AnnotationsPanelCtrl', function($scope, panelSrv, annotationsSrv, timeSrv, datasourceSrv, $q, $sce) {
+  module.controller('AnnotationsPanelCtrl', function(
+    $scope, 
+    $rootScope,
+    panelSrv, 
+    annotationsSrv, 
+    timeSrv, 
+    datasourceSrv, 
+    $q, 
+    $sce
+    ) {
+
     $scope.panelMeta = new PanelMeta({
-      description : "Annotations listing panel.",
-      index: 'General'
+      description : "Annotations panel using graphite events."
     });
     $scope.panelMeta.addEditorTab('Tags & Types', 'plugins/annotations.panel/tags-types-editor.html');
-  
+    
+    // Cached annotations
     var promiseCached;
     var list = [];
+
     var timezone;
     $scope.annotations = {};
     $scope.range = timeSrv.timeRange();
     $scope.rangeUnparsed = timeSrv.timeRange(false);
     
-    // Helper to display events in modal
-    $scope.annotationModal = function (annotation) {
-      $scope.textAsHtml = function (text) {
-        return $sce.trustAsHtml(text.replace("\r\n", "<br>"));
-      }
+    // Get includable types from config
+    function includableTypes() {
+      if (!config.graphite || !config.graphite.events) return [];
+      var c = config.graphite.events;
 
-      $scope.annotation = annotation;
+      return c.types || []; 
     }
 
     // set and populate defaults
     var _d = {
+      includeTypes: ['info', 'error', 'fatal'],
+      includableTypes: includableTypes(),
     };
     _.defaults($scope.panel, _d);
+    
+    function clearCache() {
+      promiseCached = null;
+      list = [];
+    }
+    function refresh() {
+      clearCache();
+      $scope.get_data();
+    }
+
+    // refresh when we want to.
+    $scope.$on('refresh', refresh);
+    $scope.$watch('panel.includeTypes', refresh);
+
+    // Refresh when dashboard wants to.
+    $rootScope.onAppEvent('refresh', clearCache);
+    $rootScope.onAppEvent('setup-dashboard', clearCache);
 
     // Main method that gets called every time we need to draw/refresh.
     $scope.get_data = function () {
@@ -49,18 +79,15 @@ function (angular, app, _, moment, PanelMeta) {
       
       $scope.annotationsPromise
         .then(function (annotations) {
-          // Reverse so newest will be drawn first.
-          annotations.reverse();
-
           $scope.panelMeta.loading = false;
           $scope.annotations = annotations;
-          $scope.render(annotations);
+
+          $scope.render();
         }, function () {
           $scope.panelMeta.loading = false;
           $scope.render();
         });
     };
-    $scope.$on('refresh', $scope.get_data); // Reload on refresh
 
     $scope.updateTimeRange = function () {
       $scope.range = timeSrv.timeRange();
@@ -103,35 +130,67 @@ function (angular, app, _, moment, PanelMeta) {
    
     // Analyze and enrich annotation with usefullness.
     function addAnnotation(annotation) {
-      var hrTimeFormat = 'YYYY-MM-DD HH:mm:ss';
-      var hrTimeFormatShort = 'MM-DD HH:mm';
+      // Try to parse a 'type' from the title.
+      annotation.type   = annotationType(annotation);
+
+      // Is this a 'long' event?
+      annotation.isLong = annotation.text.length > config.isLongThreshold;
+
+      // Apply inclusion rules.
+      if (!includeAnnotation(annotation)) return;
       
+      // Human readable timestamps
+      var hrTimeFormat = config.hrTimeFormat;
+      var hrTimeFormatShort = config.hrTimeFormatShort;
       var formatter = (timezone === 'browser') ? moment : moment.utc;
       annotation.hrTime = formatter(annotation.time).format(hrTimeFormat);
       annotation.hrTimeShort = formatter(annotation.time).format(hrTimeFormatShort);
-     
-      // Try to parse a 'type' from the title.
-      ['info', 'warn', 'error', 'fatal'].forEach(function (type) {
-        if (annotation.title.match(type)) {
-          annotation.type = type;
-          return;
-        }
+
+      // Add annotation in reverse order, newest first.
+      list.unshift(annotation);
+    }
+  
+    // Is the annotation(.type) included in our list?
+    function includeAnnotation(annotation) {
+      // No types included? Everything is included!
+      if (!$scope.panel.includeTypes || !$scope.panel.includeTypes.length) return true;
+
+      return $scope.panel.includeTypes.indexOf(annotation.type) !== -1;
+    }
+
+    // Determine annotation 'type' using config.
+    function annotationType(annotation) {
+      // We need a config at least.
+      if (!config.graphite.events || !config.graphite.events.defaultType) return;
+      var c     = config.graphite.events,
+          title = annotation.title,
+          type  = c.defaultType;
+
+      if (!c.typeFromTitle) return c.defaultType;
+        
+      c.typeFromTitle.forEach(function (typeMatch) {
+        if (title.match(typeMatch)) return type = typeMatch;
       });
-      
 
-      annotation.isLong = annotation.text.length > 50;
-
-      // annotation.eventId = annotation.$$hashKey.split(':')[1] + 3;
-      list.push(annotation);
+      return type;
     }
 
     function errorHandler(err) {
       console.log('Annotation error: ', err);
       var message = err.message || "Annotation query failed";
     }
+    
+    // Helper to display events in modal
+    $scope.annotationModal = function (annotation) {
+      $scope.textAsHtml = function (text) {
+        return $sce.trustAsHtml(text.replace("\r\n", "<br>"));
+      }
 
-    $scope.render = function (annotations) {
-      $scope.$broadcast('render', annotations);
+      $scope.annotation = annotation;
+    }
+
+    $scope.render = function () {
+      $scope.$broadcast('render');
     }
 
     panelSrv.init($scope);
