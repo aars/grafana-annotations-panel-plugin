e78f658c96ef113f12a1209db332f99d3964d601
Aaron Heesakkers <aaronheesakkers@gmail.com>
2015-02-05 15:42:45 +0100

    Add tagList. Add tagColors. Add better modals. Add messageFormat support.

--- module.js
+++ module.js
@@ -20,7 +20,8 @@ function (angular, app, _, moment, PanelMeta, config) {
     timeSrv,
     datasourceSrv,
     $q,
-    $sce
+    $sce,
+    $modal
     ) {
 
     $scope.panelMeta = new PanelMeta({
@@ -28,12 +29,14 @@ function (angular, app, _, moment, PanelMeta, config) {
     });
     $scope.panelMeta.addEditorTab('Filters', 'plugins/annotations.panel/filter-editor.html');
 
-    // Cached annotations
+    // Cached annotations promise and full list.
     var promiseCached;
     var list = [];
 
+    // Annotations to render.
+    $scope.annotations = [];
+
     var timezone;
-    $scope.annotations = {};
     $scope.range = timeSrv.timeRange();
     $scope.rangeUnparsed = timeSrv.timeRange(false);
 
@@ -44,15 +47,6 @@ function (angular, app, _, moment, PanelMeta, config) {
 
       return c.types || [];
     }
-
-    // set and populate defaults
-    var _d = {
-      includeTypes: ['info', 'error', 'fatal'],
-      includableTypes: includableTypes(),
-      datasource: 'graphite',
-      graphiteTags: null
-    };
-    _.defaults($scope.panel, _d);
 
     function clearCache() {
       promiseCached = null;
@@ -65,13 +59,21 @@ function (angular, app, _, moment, PanelMeta, config) {
 
     // refresh when we want to.
     $scope.$on('refresh', refresh);
-    $scope.$watch('panel.includeTypes', refresh);
-    $scope.$watch('panel.datasource', refresh);
+
+    $scope.$watch('panel.includeTypes', $scope.render);
     $scope.$watch('panel.graphiteTags', refresh);
 
-    // Refresh when dashboard wants to.
+    // We don't support other datasources yet
+    // $scope.$watch('panel.datasource', refresh);
+
+    // Clear cache when dashboard want to refresh.
     $rootScope.onAppEvent('refresh', clearCache);
     $rootScope.onAppEvent('setup-dashboard', clearCache);
+
+    $scope.updateTimeRange = function () {
+      $scope.range = timeSrv.timeRange();
+      $scope.rangeUnparsed = timeSrv.timeRange(false);
+    };
 
     // Main method that gets called every time we need to draw/refresh.
     $scope.get_data = function () {
@@ -84,18 +86,11 @@ function (angular, app, _, moment, PanelMeta, config) {
       $scope.annotationsPromise
         .then(function (annotations) {
           $scope.panelMeta.loading = false;
-          $scope.annotations = annotations;
-
           $scope.render();
         }, function () {
           $scope.panelMeta.loading = false;
           $scope.render();
         });
-    };
-
-    $scope.updateTimeRange = function () {
-      $scope.range = timeSrv.timeRange();
-      $scope.rangeUnparsed = timeSrv.timeRange(false);
     };
 
     function getAnnotations(rangeUnparsed) {
@@ -109,15 +104,18 @@ function (angular, app, _, moment, PanelMeta, config) {
 
       promiseCached = datasource.annotationQuery(annotation, rangeUnparsed)
           .then(receiveAnnotationResults)
-          .then(null, errorHandler)
-          .then(function () {
-            return list;
-          });
+          .then(null, errorHandler);
+
+      promiseCached.then(function () {
+        return list;
+      });
 
       return promiseCached;
     }
 
     function receiveAnnotationResults(results) {
+      // Clear and repopulate list.
+      list = [];
       for (var i=0; i<results.length; i++) {
         addAnnotation(results[i]);
       }
@@ -131,9 +129,6 @@ function (angular, app, _, moment, PanelMeta, config) {
       // Is this a 'long' event?
       annotation.isLong = annotation.text.length > config.isLongThreshold;
 
-      // Apply inclusion rules.
-      if (!includeAnnotation(annotation)) return;
-
       // Human readable timestamps
       var hrTimeFormat = config.hrTimeFormat;
       var hrTimeFormatShort = config.hrTimeFormatShort;
@@ -141,8 +136,42 @@ function (angular, app, _, moment, PanelMeta, config) {
       annotation.hrTime = formatter(annotation.time).format(hrTimeFormat);
       annotation.hrTimeShort = formatter(annotation.time).format(hrTimeFormatShort);
 
+      // Try to find message/data.
+      parseAnnotationText(annotation);
+
+      // parse tagstring.
+      parseAnnotationTags(annotation);
+
       // Add annotation in reverse order, newest first.
       list.unshift(annotation);
+    }
+
+    // Seperate tags string into seperate tags
+    function parseAnnotationTags(annotation) {
+      // We support comma and space seperated.
+      var seperator = (annotation.tags.indexOf(',') !== -1) ? ',' : ' ';
+      annotation.tagList = annotation.tags.split(seperator);
+    }
+
+    // Try to parse {message: 'message', data: {}} format.
+    function parseAnnotationText(annotation) {
+      var message = annotation.text, data, dataString;
+      try {
+        var json = JSON.parse(annotation.text);
+
+        message    = json.message;
+        data       = json.data;
+        dataString = data;
+
+        // try to prettify the dataString if it looks like JSON
+        try {
+          dataString = JSON.stringify(JSON.parse(data), null, '  ');
+        } catch (e) {}
+      } catch (e) {}
+
+      annotation.message    = message;
+      annotation.data       = data;
+      annotation.dataString = dataString;
     }
 
     // Is the annotation(.type) included in our list?
@@ -175,18 +204,55 @@ function (angular, app, _, moment, PanelMeta, config) {
       var message = err.message || "Annotation query failed";
     }
 
+    function filterAnnotations() {
+      return list.filter(function (annotation) {
+        return includeAnnotation(annotation);
+      });
+    }
+
+    // set and populate defaults
+    var _d = {
+      includeTypes: [],
+      includableTypes: includableTypes(),
+      datasource: 'graphite',
+      graphiteTags: null
+    };
+    _.defaults($scope.panel, _d);
+
     // Helper to display events in modal
     $scope.annotationModal = function (annotation) {
-      $scope.textAsHtml = function (text) {
-        return $sce.trustAsHtml(text.replace("\r\n", "<br>"));
-      }
+      var scope = $rootScope.$new();
 
-      $scope.annotation = annotation;
+      scope.tagColor = $scope.tagColor;
+      scope.annotation = annotation;
+      var modal = $modal({
+        template: 'plugins/annotations.panel/annotation-modal.html',
+        modalClass: 'testclass',
+        //persist: true,
+        show: false,
+        scope: scope,
+        keyboard: false
+      });
+
+      $q.when(modal).then(function (modalEl) {
+        modalEl.modal('show');
+      });
     }
 
+    $scope.tagColor = function (tag) {
+      var stringHexNumber = (
+          parseInt(
+            parseInt(tag, 36).toExponential().slice(2,-5)
+          , 10) & 0xFFFFFF
+        ).toString(16).toUpperCase();
+
+      return '#' + ('000000' + stringHexNumber).slice(-6);
+    }
     $scope.render = function () {
+      $scope.annotations = filterAnnotations();
       $scope.$broadcast('render');
     }
+
 
     panelSrv.init($scope);
   });
